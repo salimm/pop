738ad4d2dcd739ed6c80f7efa27a0076d140259e
salimm <salimm@cs.pitt.edu>
2018-02-05 13:49:35 -0500

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 setup(
   name = 'pylods',
   packages = ['pylods'], # this must be the same as the name above
-  version = '0.3.3',
+  version = '0.3.4',
   description = 'Pylods, is a python library for object deserialization and serialization. Pylods provides tools to allow automatic recursive serialization of class instances and implementation of customizes rs and deserializers similar to JacksonJson for java. It currently supports JSON and Msgpack',
   author = 'Salim Malakouti',
   author_email = 'salim.malakouti@gmail.com',
   license = 'MIT',
   url = 'https://github.com/salimm/pylods', # use the URL to the github repo
-  download_url = 'http://github.com/salimm/pylods/archive/0.3.3.tar.gz', # I'll explain this in a second
+  download_url = 'http://github.com/salimm/pylods/archive/0.3.4.tar.gz', # I'll explain this in a second
   keywords = ['python','serialization','deserialization','paser','json','msgpack','object oriented','fast','extendable','type based','jackson json'], # arbitrary keywords
   classifiers = ['Programming Language :: Python'],
   install_requires=['enum34'],
